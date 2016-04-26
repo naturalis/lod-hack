2befb8bcb406e1b594bc94528abab87e648635e4
Ayco Holleman <ayco.holleman@naturalis.nl>
2016-04-26 11:28:46 +0200

    Show NHM input and mangled ouput

--- naturalis/NHMTest.java
+++ naturalis/NHMTest.java
@@ -14,6 +14,7 @@ import nl.naturalis.nda.domain.Specimen;
 import nl.naturalis.nda.domain.SpecimenIdentification;
 import nl.naturalis.nda.domain.TaxonomicRank;
 
+import org.domainobject.util.FileUtil;
 import org.domainobject.util.debug.BeanPrinter;
 import org.domainobject.util.debug.Debug;
 import org.domainobject.util.http.SimpleHttpGet;
@@ -21,18 +22,26 @@ import org.domainobject.util.http.SimpleHttpGet;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * Tests making a call to NHM and converting the response to the NBA data model. This code
+ * has also been inserted into the Specimen DAO in the LOD-pilot git branch.
+ * 
+ * @author ayco
+ *
+ */
 public class NHMTest {
 
 	public static void main(String[] args) throws Exception
 	{
 		SimpleHttpGet nhm = new SimpleHttpGet();
-		nhm.setBaseUrl("http://data.nhm.ac.uk/api/action/datastore_search?resource_id=05ff2255-c38a-40c9-b657-4ccb55ab2feb&q=egg");
+		nhm.setBaseUrl("http://data.nhm.ac.uk/api/action/datastore_search?resource_id=05ff2255-c38a-40c9-b657-4ccb55ab2feb&q=egg&limit=3");
 		nhm.execute();
-		byte[] res = nhm.getResponseBody();
+		byte[] response = nhm.getResponseBody();
 		ObjectMapper om = new ObjectMapper();
 		TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {};
-		HashMap<String, Object> map = om.readValue(res, typeRef);
+		HashMap<String, Object> map = om.readValue(response, typeRef);
 		Map result = (Map) map.get("result");
+		Debug.print("/home/ayco/input.txt", BeanPrinter.toString(result));
 		List records = (List) result.get("records");
 		Iterator iterator = records.iterator();
 		List<Specimen> specimens = new ArrayList<>();
@@ -43,26 +52,25 @@ public class NHMTest {
 			specimen.setUnitID(get(data, "occurrenceID"));
 			specimen.setRecordBasis(get(data, "basisOfRecord"));
 			ScientificName sn = new ScientificName();
-			String s = "scientificName";
 			sn.setFullScientificName(get(data, "scientificName"));
 			sn.setAuthorshipVerbatim(get(data, "scientificNameAuthorship"));
-			SpecimenIdentification si = new SpecimenIdentification();
-			si.setScientificName(sn);
+			SpecimenIdentification identification = new SpecimenIdentification();
+			identification.setScientificName(sn);
 			List<Monomial> systemClassification = new ArrayList<>();
 			systemClassification.add(new Monomial(TaxonomicRank.KINGDOM, get(data, "kingdom")));
 			systemClassification.add(new Monomial(TaxonomicRank.PHYLUM, get(data, "phylum")));
 			systemClassification.add(new Monomial(TaxonomicRank.ORDER, get(data, "order")));
 			systemClassification.add(new Monomial(TaxonomicRank.FAMILY, get(data, "family")));
 			systemClassification.add(new Monomial(TaxonomicRank.GENUS, get(data, "genus")));
-			specimen.addIndentification(si);
+			List<SpecimenIdentification> identifications = new ArrayList<>();
+			identifications.add(identification);
+			specimen.setIdentifications(identifications);
 			specimens.add(specimen);
 			DefaultClassification dc = DefaultClassification
 					.fromSystemClassification(systemClassification);
-			si.setDefaultClassification(dc);
-
-			break;
+			identification.setDefaultClassification(dc);
 		}
-		Debug.print("/home/ayco/test.txt", BeanPrinter.toString(specimens));
+		Debug.print("/home/ayco/output.txt", BeanPrinter.toString(specimens));
 
 	}
 
