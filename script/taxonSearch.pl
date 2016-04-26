5984a151089258a3ea3e4202a42b1072757292da
Rutger Vos <rutgeraldo@gmail.com>
2016-04-26 09:08:15 +0200

    Update taxonSearch.pl

--- script/taxonSearch.pl
+++ script/taxonSearch.pl
@@ -2,23 +2,15 @@
 use strict;
 use warnings;
 use Getopt::Long;
-use Data::Dumper;
-use Bio::Phylo::IO 'unparse';
-use Bio::Phylo::Util::Logger ':levels';
 use Bio::Phylo::PhyloWS::Client::NBA::TaxonSearch;
 
 # process command line arguments
 my $query;
-my $format;
-my $verbosity = WARN;
 GetOptions(
 	'query=s'  => \$query,
-	'format=s' => \$format,
-	'verbose+' => \$verbosity,
 );
 
 # instantiate helper objects
-my $log = Bio::Phylo::Util::Logger->new( '-level' => $verbosity );
 my $ts  = Bio::Phylo::PhyloWS::Client::NBA::TaxonSearch->new;
 
 # run the query
